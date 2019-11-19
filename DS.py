c6297a61d6d2bf9674b6159655d48a16148c63b3
youpeterabb1t <21300259@handong.edu>
2019-11-19 02:19:32 +0000

    data structure

---
+++ DS.py
@@ -0,0 +1,39 @@
+class User(object):
+	id=""
+	stuid=""
+	#name=""
+	rdate="2019-11-19"
+	rstarttime="0800"
+	rendtime="1000"
+	pid=""
+	membergrade=""
+	point=""
+	coworker=""
+	progress=""
+
+class UserManager(object):
+	pass
+
+class Equip(object):
+	id=""
+	spec=""
+
+class EquipManager(object):
+	'''
+
+	'''
+	pass
+
+class History():
+	pass	
+
+class reservestat(object):
+	
+'''
+장비신청상황
+학생id, 장비 ID, 신청날짜, 장비사용날짜, 사용금액  
+'''
+	stat={}
+
+	def addstat():
+		pass
