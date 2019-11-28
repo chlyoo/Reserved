24fbd31cba40aecf1fa1a5721666f1f20a9aa248
Changbeom Choi <cbchoi@handong.edu>
2019-11-28 08:30:01 +0000

    main

--- app/main/views.py
+++ app/main/views.py
@@ -25,6 +25,7 @@ def index():
 		form.name.data = ''
 		return redirect(url_for('.index'))
 	return render_template('index.html',
+							equip_lst = ['a', 'b', 'c'],
 							form=form, name=session.get('name'),
 							known=session.get('known', False),
 							current_time=datetime.utcnow())
