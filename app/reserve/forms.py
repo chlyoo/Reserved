889481d547cfef192d792aa5ba899c513f9f84c6
youpeterabb1t <21300259@handong.edu>
2019-11-29 02:17:50 +0000

    2019-11-29

--- app/reserve/forms.py
+++ app/reserve/forms.py
@@ -6,38 +6,8 @@ from wtforms import ValidationError
 from ..models import User
 from .. import db
 
-class LoginForm(FlaskForm):
-	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-	password = PasswordField('Password', validators=[Required()])
-	remember_me = BooleanField('Keep me logged in')
-	submit = SubmitField('Log In')
+from wtforms import FileField
 
-
-class RegistrationForm(FlaskForm):
-	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-
-
-	username = StringField('Username',
-                       validators=[Required(), Length(1, 64), Regexp('^[가-힣A-Za-z][가-힣A-Za-z0-9_.]*$', 0,
-							'Usernames must have only letters, '
-							'numbers, dots or underscores')])
-	stuid = StringField('Stuid', validators=[Required(), Length(5, 8)])
-	password = PasswordField('Password', validators=[Required(), 
-													EqualTo('password2', 
-													message='Passwords must match.')])
-	password2 = PasswordField('Confirm password', validators=[Required()])
-	submit = SubmitField('Register')
-
-	def validate_email(self, field):
-		collection = db.get_collection('users')
-		results = collection.find_one({'id':field.data})
-		if results is not None:
-			raise ValidationError('Email already registered.')
-		pass
-	
-	def validate_username(self, field):
-		collection = db.get_collection('users')
-		results = collection.find_one({'username':field.data})
-		if results is not None:
-			raise ValidationError('Username already registered.')
-		pass
+class RequestRegisterForm(FlaskForm):
+    file = FileField('Upload an 3D printing file', validators=[Required()])
+    submit = SubmitField('Submit')
--- app/reserve/views.py
+++ app/reserve/views.py
@@ -9,6 +9,32 @@ from ..email import send_email # added 20191108
 
 from flask_login import current_user
 
-@reserve.route('/', methods=['GET', 'POST'])
+"""@reserve.route('/', methods=['GET', 'POST'])
 def reserve_main():
 	return "Hello"
+
+"""
+from werkzeug import secure_filename
+from .. import fs
+from flask import send_file
+
+@reserve.route('/', methods=['GET', 'POST'])
+def show_images():# 이부분을 프린터 띄우는 코드로 수정하기
+	return render_template('reserve/img_lst.html',
+		file_lst = [url_for('img.image', filename=file) for file in fs.list()])
+
+@reserve.route('/register', methods=['GET', 'POST'])
+def register_images():
+    form = RequestRegisterForm()
+    if form.validate_on_submit():
+        filename = secure_filename(form.file.data.filename)
+        oid = fs.put(form.file.data, content_type=form.file.data.content_type, filename=filename)
+        print(filename)
+        return redirect(url_for('img.show_images'))
+    return render_template('img/img_register.html', form=form)
+
+
+@reserve.route('/workspace/<filename>')
+def image(filename):
+    gridout = fs.get_last_version(filename=filename)
+    return send_file(gridout, mimetype=gridout.content_type)
