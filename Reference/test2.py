90e74187556d602a70103e3e6c83978ce6ef8693
youpeterabb1t <21300259@handong.edu>
2019-12-05 17:14:40 +0000

    02:14

--- Reference/test2.py
+++ Reference/test2.py
@@ -1,14 +1,28 @@
-from datetime import datetime, timedelta, time
-import calendar
-rdate=[0 for i in range(26)]
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = SearchitemForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ''
 
-"""for day in range(7):
-    print("dat")
-    for hour in range(9,21,1):
-        for min in range(2):
-            print(str(hour).zfill(2)+":"+str(30*min).zfill(2))
+        '''
+        {file:url_for(main.image', filename=file) for file in fs.list()}
+        '''
+        return redirect(url_for('.index'))
 
-"""
-i= calendar.calendar(datetime.now().year)
-calendar.i
-print(i)
+    collection = db.get_collection('users')
+    results = collection.find({'user':current_user.id})
+    collection = db.get_collection('items')
+    collection.update({}, {'$set': {'participation': 'no'}})
+    if results != None:
+        collection.update_one({'participation_uid': current_user.id}, {'$set': {'participation': 'yes'}})
+
+    return render_template('index.html',
+                           item_list=[i for i in db.get_collection('items').find()],
+                           file_lst={file: url_for('main.image', filename=file) for file in fs.list()},
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False),
+                           current_time=datetime.utcnow())
